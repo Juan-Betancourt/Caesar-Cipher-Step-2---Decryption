bb8a7b3b354a5e9ee6e224cb18320be05e1cbe4d
Juan Betancourt <juancbetancourt11@gmail.com>
2022-05-10 20:59:20 +0000

    Completed Decode of caesar cipher step 2

--- main.py
+++ main.py
@@ -4,24 +4,25 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt (plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
-encrypt(plain_text=text, shift_amount=shift)
+if direction == "encode":
+  def encrypt(plain_text, shift_amount):
+      cipher_text = ""
+      for letter in plain_text:
+          position = alphabet.index(letter)
+          new_position = position + shift_amount
+          new_letter = alphabet[new_position]
+          cipher_text += new_letter
+      print(f"The encoded text is {cipher_text}")
+  encrypt(plain_text=text, shift_amount=shift)
 
-#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+elif direction == "decode":
+  def decrypt(encode_text, shift_number):
+      cipher_text = ""
+      for letter in encode_text:
+          position = alphabet.index(letter)
+          new_position = position - shift_number
+          new_letter = alphabet[new_position]
+          cipher_text += new_letter
+      print(f"The decode text is {cipher_text}")
+  decrypt(encode_text=text, shift_number=shift)
 
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
-  #e.g. 
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
-
-
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
